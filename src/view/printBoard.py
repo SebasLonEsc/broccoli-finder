adb26dcaa9c1838ad7752faac573c55eac80604f
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-06-24 20:35:14 -0500

    Added module for printing board on console

---
+++ src/view/printBoard.py
@@ -0,0 +1,55 @@
+from src.logic.handleMove import handleMove
+
+def printing(boardObject):
+  guideRow = "     "
+  separationRow = "   --"
+  for j in range(boardObject.tilesBoard.shape[1]):
+    complement = "  "
+    if j+1 >= 10:
+      complement= " "
+
+    guideRow += str(j + 1) + complement
+    separationRow += "---"
+
+  print(guideRow)
+  print(separationRow)
+
+  for i in range(boardObject.tilesBoard.shape[0]):
+    complement = " "
+
+    if i+1 >= 10:
+      complement = ""
+
+    row = complement + str(i + 1) + " | "
+    for j in range(boardObject.tilesBoard[i].shape[0]):
+      row += boardObject.tilesBoard[i,j]["tileValue"] + "  "
+
+    row +="| " +  str(i + 1)
+    print(row)
+
+  print(separationRow)
+  print(guideRow)
+
+def printBoardOnConsole(boardObject):
+  gameStatus = 0
+  rowLimit = boardObject.totalRows
+  columnLimit = boardObject.totalColumns
+
+  while gameStatus == 0:
+    row = int(input("Enter the row:")) - 1
+    column = int(input("Enter the column:")) - 1
+
+    if  row < 0 or row >= rowLimit or column < 0 or column >= columnLimit:
+      print("Invalid Row or column value")
+      continue
+
+    boardObject, gameStatus = handleMove(boardObject, [row, column])
+    printing(boardObject)
+
+  if gameStatus == 1:
+    print("CONGRATULATIONS")
+    return 0
+  
+  if gameStatus == -1:
+    print("Sorry you lost")
+    return 0
