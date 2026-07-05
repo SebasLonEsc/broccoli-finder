1ee9ed8d30507ae30582f96daca8208277ceca33
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-07-04 19:57:39 -0500

    Added function to verify win or lose condition and change the interface accordingly

--- src/view/boardInterface.py
+++ src/view/boardInterface.py
@@ -1,8 +1,33 @@
 import tkinter as tk
-from functools import partial
 import numpy as np
 import math
+from functools import partial
 from src.logic.handleMove import handleMove
+from src.logic.constants.gameValues import *
+
+def handleGameStatus(boardObject, buttons, movePosition):
+  tilesBoard = boardObject.tilesBoard
+  for row in range(0, boardObject.totalRows):
+    for column in range(0, boardObject.totalColumns):
+      checked = tilesBoard[row, column]["checked"]
+      isBroccoli = tilesBoard[row, column]["tileValue"] == "-1"
+      backgroundColor = "gray70"
+      fontColor = "white"
+
+      buttons[row, column].config(state=tk.DISABLED)
+
+      if movePosition[0] == row and movePosition[1] == column:
+        backgroundColor = "dark red"
+        fontColor = "white"
+      else:
+        backgroundColor = "gray50"
+        fontColor = "dark red"
+
+      if checked and isBroccoli:
+        buttons[row, column].config(bg=backgroundColor)
+        buttons[row, column].config(disabledforeground=fontColor)
+        buttons[row, column].config(text="-1")
+
 
 def handleText(boardObject, buttons):
   tilesBoard = boardObject.tilesBoard
@@ -26,9 +51,18 @@ def handleText(boardObject, buttons):
         buttons[row, column].config(bg="gray70")
         buttons[row, column].config(text=text)
 
-def handleClick(boardObject, buttons, movePosition):
+def handleClick(boardObject, buttons, movePosition, winLabel):
   boardObject, gameStatus = handleMove(boardObject, movePosition)
   handleText(boardObject, buttons)
+
+  if gameStatus != 0:
+    gameStatusText = WINNINGTEXT
+
+    if gameStatus < 0:
+      gameStatusText = LOSINGTEXT
+
+    winLabel.config(text=gameStatusText)
+    handleGameStatus(boardObject, buttons, movePosition)
 
 def createInterface(boardObject):
   rows = boardObject.totalRows
@@ -38,8 +72,13 @@ def createInterface(boardObject):
 
   root = tk.Tk()
   root.title("Broccoli")
-  label = tk.Label(root, text="Broccoli Seeker", anchor="center")
-  label.grid(row=0, column=math.floor(columns/2)-2, columnspan=4)
+  tk.Label(
+    root,
+    text="Broccoli Seeker",
+    anchor="center").grid(row=0, column=math.floor(columns/2)-2, columnspan=4)
+  
+  label = tk.Label(root, text="", anchor="center")
+  
 
   for row in range(0, rows):
     for column in range(0, columns):
@@ -56,7 +95,7 @@ def createInterface(boardObject):
                         anchor="center",
                         bd=1,
                         bg=backgroundColor,
-                        command= partial(handleClick, boardObject, buttons, [row, column]),
+                        command= partial(handleClick, boardObject, buttons, [row, column], label),
                         disabledforeground="white",
                         justify="center",
                         height=1,
@@ -71,5 +110,7 @@ def createInterface(boardObject):
       buttons[row, column] = button
       button.grid(row=row+1, column=column)
       button = None
+
+  label.grid(row=rows+2, column=math.floor(columns/2)-2, columnspan=4)
   
   root.mainloop()
