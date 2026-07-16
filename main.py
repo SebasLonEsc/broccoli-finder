5b8dced14013f1fdbd696c54f1727877f4666ffd
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-07-15 22:01:05 -0500

    Rearranged game type selector to main due to cyclical import error

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from src.logic.board import handleBoardGeneration
+from src.view.printBoard import printBoardOnConsole
+from src.view.mainMenuView import createMainMenuView
+from src.logic.board import boardGenerator
+
+# Handles the generation of the Board and redirects to the specific game interface
+# Input:
+#   gameType: The type of interface used for the game
+#     1: Play the game in console
+#     2: Play the game in an interface
+# Output:
+#   Nothing
+def selectGameType(gameType):
+  if gameType == 1:
+    rows = int(input("Enter the rows:"))
+    columns = int(input("Enter the columns:"))
+    broccoliAmount = int(input("Enter the number of broccolis:"))
+    boardObject = boardGenerator(rows, columns, broccoliAmount)
+    printBoardOnConsole(boardObject)
+
+  else:
+    createMainMenuView()
 
 def main():
   invalidGameType = True
@@ -17,6 +37,6 @@ def main():
     else:
       print("\nInvalid game type")
 
-  handleBoardGeneration(gameType)
+  selectGameType(gameType)
 
 main()
