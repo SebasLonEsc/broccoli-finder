5097c7e1cca2757cef4de2d4aa32047b26b21c24
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-07-21 23:07:54 -0500

    Added constants for game difficulties and board sizes types

--- src/logic/constants/gameValues.py
+++ src/logic/constants/gameValues.py
@@ -3,3 +3,14 @@ WINNINGTEXT = "CONGRATULATIONS\nYou Won the Game!!"
 
 # Display text when losing the game
 LOSINGTEXT = "Sorry you lost"
+
+# Game Difficulties types
+GAMEDIFFICULTIES = ["Easy", "Normal", "Hard"]
+
+# The maximun amount of broccolis per game difficulty
+# The total is found using the total amount of tiles in the board
+GAMEBROCCOLIPERCENTS = {
+  "Easy": [0.05, 0.1],
+  "Normal": [0.12, 0.2],
+  "Hard": [0.22, 0.3]
+  }
