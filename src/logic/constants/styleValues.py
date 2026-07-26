455402922a031051a9bcc329909665b0182fce5d
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-07-25 22:20:46 -0500

    Added file with all background and font colors defined

---
+++ src/logic/constants/styleValues.py
@@ -0,0 +1,22 @@
+PROXIMITYCOLORS = ["#ffffff",
+                   "#00828C",
+                   "#002A8C",
+                   "#008C35",
+                   "#FFD70A",
+                   "#FF6400",
+                   "#C80A00",
+                   "#8C0054",
+                   "#4D008C"
+                  ]
+
+TILEBACKGROUNDCOLOR = "#D3D3D3"
+
+NULLSPACETILECOLOR = "#8C8C8C"
+
+DISABLEDTILECOLOR = "#B3B3B3"
+
+BROCCOLITILECOLOR = "#7F7F7F"
+
+EATENBROCCOLITILECOLOR = "#8B0000"
+
+BUTTONCOLOR = "#D3D3D3"
