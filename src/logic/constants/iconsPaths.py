e59c1f68c7befd83a09dfda1022637a4c83680f4
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-07-26 20:09:17 -0500

    Added comments for images paths and style values

--- src/logic/constants/iconsPaths.py
+++
@@ -1,9 +0,0 @@
-PROXIMITYNUMBERPATHS = ["Disabled_Empty_Tile.png",
-                        "One_Tile.png",
-                        "Two_Tile.png",
-                        "Three_Tile.png",
-                        "Four_Tile.png",
-                        "Five_Tile.png",
-                        "Six_Tile.png",
-                        "Seven_Tile.png",
-                        "Eight_Tile.png",]
--- src/logic/constants/styleValues.py
+++ src/logic/constants/styleValues.py
@@ -1,3 +1,4 @@
+# The broccoli proximity number tile colors
 PROXIMITYCOLORS = ["#ffffff",
                    "#00828C",
                    "#002A8C",
@@ -9,18 +10,29 @@ PROXIMITYCOLORS = ["#ffffff",
                    "#4D008C"
                   ]
 
+# Normal tile background color
 TILEBACKGROUNDCOLOR = "#D3D3D3"
 
+# NullSpace tile background color
 NULLSPACETILECOLOR = "#8C8C8C"
 
+# Disabled tile background color
 DISABLEDTILECOLOR = "#B3B3B3"
 
+# Broccoli tile background color
 BROCCOLITILECOLOR = "#7F7F7F"
 
+# Eaten broccoli tile background color
+# Indicates the tile where the broccoli was found upon losing the game
 EATENBROCCOLITILECOLOR = "#8B0000"
 
+# Any action button background color
 BUTTONCOLOR = "#D3D3D3"
 
+# Any action button active background color
+BUTTONACTIVECOLOR = "#FFFFFF"
+
+# A dictionary with for the [width, height] values for the board size selector buttons
 BOARDBUTTONSIZES = {
   "Small": [216, 168],
   "Medium": [216, 216],
