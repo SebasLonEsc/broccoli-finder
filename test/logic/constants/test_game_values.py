95d12c516f395d94f55083363129221c9936d6ed
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-08-25 17:39:08 -0500

    Added testing for new end game text function

--- test/logic/constants/test_game_values.py
+++ test/logic/constants/test_game_values.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import patch, Mock
 
-from src.logic.constants.gameValues import get_random_text
+import src.lang.language as Lg
+from src.logic.constants.gameValues import get_random_text, get_end_game_text
 
 class TestRandomText(unittest.TestCase):
   def test_single_text_random(self):
@@ -14,7 +16,6 @@ class TestRandomText(unittest.TestCase):
   def test_multiple_texts_random(self):
     text_array = ["Hello", "World", "Happy", "Coding"]
     returned_text = get_random_text(text_array)
-
     valid_text = False
 
     for text in text_array:
@@ -24,3 +25,31 @@ class TestRandomText(unittest.TestCase):
 
     self.assertTrue(valid_text,
                     "Returned text should be from the array sample")
+
+class TestEndGameText(unittest.TestCase):
+  def test_single_end_game_text(self):
+    text_array = ["Hello World"]
+    mock_dir = {"test_value": text_array}
+
+    with patch.dict(Lg.lang, mock_dir):
+      returned_text = get_end_game_text("test_value")
+
+      self.assertEqual(returned_text,
+                       text_array[0],
+                       "Returned text should be equal to the one in array sample")
+
+  def test_multiple_end_game_text(self):
+    text_array = ["Hello", "World", "Happy", "Coding"]
+    mock_dir = {"test_value": text_array}
+
+    with patch.dict(Lg.lang, mock_dir):
+      returned_text = get_end_game_text("test_value")
+      valid_text = False
+      
+      for text in text_array:
+        if text == returned_text:
+          valid_text = True
+          break
+  
+      self.assertTrue(valid_text,
+                      "Returned text should be from the array sample")
