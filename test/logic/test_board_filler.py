3edd8e59f894e859d5500bf15539124bc8968617
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-08-25 15:51:23 -0500

    Added testing for add rainbow broccoli func

--- test/logic/test_board_filler.py
+++ test/logic/test_board_filler.py
@@ -2,11 +2,14 @@ import random
 import unittest
 from unittest.mock import patch, Mock
 
-from src.logic.boardFiller import (validate_rainbow_broccoli_chance)
+from src.logic.board import Board
+from src.logic.boardFiller import (validate_rainbow_broccoli_chance,
+                                   add_rainbow_broccoli)
 from src.logic.constants.gameValues import (RAINBOW_BROCCOLI_PROPORTION_CHANCES,
                                             RAINBOW_BROCCOLI_CHANCE,
                                             MINIMUN_BROCCOLI_AMOUNT_FOR_RAINBOW_BROCCOLI
                                             )
+from src.logic.constants.boardValues import GET_BOARD_VALUE
 
 class TestRainbowBroccoliChanceValidator(unittest.TestCase):
   def test_small_amount_of_broccolis(self):
@@ -40,3 +43,27 @@ class TestRainbowBroccoliChanceValidator(unittest.TestCase):
     broccoli_proportion = 0.01 # 1% of the board is a broccoli
     add_rainbow_broccoli = validate_rainbow_broccoli_chance(broccoli_proportion, broccoli_amount)
     self.assertFalse(add_rainbow_broccoli, "Rainbow broccoli should not be added")
+
+class TestAddRainbowBroccoli(unittest.TestCase):
+  def setUp(self):
+    self.board_object = Board(8, 6)
+    self.broccoli_positions = [[1,1], [0,0]]
+
+  def test_add_rainbow_broccoli(self):
+    new_board = add_rainbow_broccoli(self.board_object.board,
+                                     self.broccoli_positions,
+                                     self.board_object.total_rows,
+                                     self.board_object.total_columns)
+
+    board_value = GET_BOARD_VALUE["blankSpace"]
+    for pos in self.broccoli_positions:
+      if new_board[pos[0], pos[1]] != GET_BOARD_VALUE["blankSpace"]:
+        board_value = new_board[pos[0], pos[1]]
+
+    self.assertNotEqual(GET_BOARD_VALUE["blankSpace"],
+                        board_value,
+                        "The position of the rainbow broccoli should not be a blankspace value")
+    
+    self.assertEqual(GET_BOARD_VALUE["rainbowBroccoli"],
+                     board_value,
+                     "Rainbow broccoli not added in board array")
