8b0819da4e65e25dcaf5ccecc855fcc0b0708fda
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-08-30 19:09:19 -0500

    Added testing to center window function

--- test/logic/test_interface_tools.py
+++ test/logic/test_interface_tools.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 import tkinter as tk
 
-from src.logic.interfaceTools import open_pillow_image
+from src.logic.interfaceTools import open_pillow_image, center_window
 from src.logic.constants.imagesPaths import IMAGES_FOLDER, GREEN_BROCCOLI_TILE_IMAGE
 
 class TestOpenPillowImage(unittest.TestCase):
@@ -33,3 +33,49 @@ class TestOpenPillowImage(unittest.TestCase):
     self.assertEqual(tk_image.height(),
                      new_height,
                      "The height should change after image opening")
+
+  def tearDown(self):
+    self.root.destroy()
+
+class TestCenterWindow(unittest.TestCase):
+  def setUp(self):
+    self.root = tk.Tk()
+    self.root_width = 100
+    self.root_height = 50
+    root_xy = 0
+    root_geometry = (str(self.root_width)
+                     + "x"
+                     + str(self.root_height)
+                     + "+"
+                     + str(root_xy)
+                     + "+"
+                     + str(root_xy))
+
+    self.root.minsize(self.root_width, self.root_height)
+    self.root.maxsize(self.root_width, self.root_height)
+    self.root.geometry(root_geometry)
+    self.root.update()
+
+  def test_center_window(self):
+    screen_width = self.root.winfo_screenwidth()
+    screen_height = self.root.winfo_screenheight()
+    expected_x_coords = (screen_width - self.root_width) // 2 # Screen center
+    expected_y_coords = (screen_height - self.root_height) // 2 # Screen center
+
+    center_window(self.root, self.root_width, self.root_height)
+    self.root.update()
+
+    new_geometry = self.root.geometry()
+    separator_position = new_geometry.find("+") # In the geometry, the first + separates size to coordinates
+    new_coordinates = new_geometry[separator_position+1:len(new_geometry)].split("+") # Gets the width and height
+
+    self.assertEqual(int(new_coordinates[0]),
+                     expected_x_coords,
+                     "Screen should be horizontally centered")
+
+    self.assertEqual(int(new_coordinates[1]),
+                     expected_y_coords,
+                     "Screen should be vertically centered")
+
+  def tearDown(self):
+    self.root.destroy()
