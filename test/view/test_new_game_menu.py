3724f3bbedc884375de33fd05b0e1b791dc5a6a5
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-09-02 20:07:07 -0500

    Added testing function to create board size selector button

--- test/view/test_new_game_menu.py
+++ test/view/test_new_game_menu.py
@@ -2,10 +2,13 @@ import unittest
 import tkinter as tk
 from tkinter import ttk
 from unittest.mock import patch
+from PIL import Image, ImageTk
 
 import src.lang.language as Lg
 from src.logic.constants.boardValues import BOARD_SIZES
-from src.view.newGameMenu import open_custom_game_view, create_game
+from src.view.newGameMenu import (open_custom_game_view,
+                                  create_game,
+                                  create_board_size_selector_button)
 
 class TestOpenCustomGameView(unittest.TestCase):
   def setUp(self):
@@ -33,7 +36,7 @@ class TestCreateGameView(unittest.TestCase):
     self.game_difficulty.pack()
     self.game_difficulty.set(Lg.lang["GameDifficulties"][0])
 
-  def test_handle_new_game(self):
+  def test_create_game(self):
     with patch("src.view.newGameMenu.create_board_interface") as mocked_function:
       mock_go_back = lambda: None
       create_game(self.label,# Func closes the widget send as arg
@@ -47,6 +50,45 @@ class TestCreateGameView(unittest.TestCase):
                        "Label should be destroyed")
 
       mocked_function.assert_called_once()
+
+  def tearDown(self):
+    self.root.destroy()
+
+class TestCreateBoardSizeSelectorButton(unittest.TestCase):
+  def setUp(self):
+    self.root = tk.Tk()
+    self.label = tk.Label(self.root)
+    self.game_difficulty = ttk.Combobox(self.root,
+                                        values=Lg.lang["GameDifficulties"],
+                                        state="readonly")
+    self.game_difficulty.pack()
+    self.game_difficulty.set(Lg.lang["GameDifficulties"][0])
+
+  def test_create_board_size_selector_button(self):
+    with (patch.object(Image, "open") as pillow_image,
+          patch.object(pillow_image, "resize") as mock_resized_image,
+          patch.object(ImageTk, "PhotoImage") as mock_photo_image):
+      mock_go_back = lambda: None
+      mock_resized_image.return_value = ""
+      mock_photo_image.return_value = None
+      board_size = BOARD_SIZES[0]
+
+      button = create_board_size_selector_button(self.root,
+                                                 mock_go_back,
+                                                 mock_go_back,
+                                                 self.root,
+                                                 self.game_difficulty,
+                                                 board_size)
+
+      self.assertTrue(button.winfo_exists,
+                      "Button should exists")
+
+      button_text = button.cget("text")
+      expected_button_text = Lg.lang[board_size]
+
+      self.assertEqual(button_text,
+                       expected_button_text,
+                       "Button text should be " + expected_button_text)
 
   def tearDown(self):
     self.root.destroy()
