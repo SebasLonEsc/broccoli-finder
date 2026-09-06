320e72d2d074d72728cb112ecb359a5e394ac08b
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-09-06 18:11:33 -0500

    Added testing of function that validates inputs of custom game menu on invalid row and column values

--- test/view/test_new_custom_game_menu.py
+++ test/view/test_new_custom_game_menu.py
@@ -3,9 +3,12 @@ import tkinter as tk
 from unittest.mock import patch
 
 import src.lang.language as Lg
+from src.logic.constants.boardValues import BOARD_SIZE_VALUES
 from src.view.newCustomGameMenu import validate_inputs
 
 class TestValidateInputs(unittest.TestCase):
+  def setUp(self):
+    self.base_error_message = Lg.lang["InputErrorText"]
   class MockSpinBox():
     def __init__(self, value):
       self.str_numeric_value = value
@@ -38,3 +41,65 @@ class TestValidateInputs(unittest.TestCase):
     self.assertEqual(expected_error_message,
                      message,
                      "Error message should be " + expected_error_message)
+
+  def test_invalid_row_input(self):
+    board_size_lower_limit = BOARD_SIZE_VALUES["Small"][0]
+    board_size_upper_limit = BOARD_SIZE_VALUES["Big"][1]
+    numeric_value = self.MockSpinBox(str(board_size_lower_limit))
+    invalid_lower_limit_value = self.MockSpinBox(str(board_size_lower_limit - 1))
+
+    expected_lower_row_value_message = (self.base_error_message
+                                        + "\n"
+                                        + Lg.lang["InvalidRowLowerLimit"]
+                                        + str(board_size_lower_limit))
+
+    message = validate_inputs(invalid_lower_limit_value,
+                              numeric_value,
+                              numeric_value)
+    self.assertEqual(expected_lower_row_value_message,
+                     message,
+                     "Invalid lower row limit value error message")
+
+    invalid_upper_limit_value = self.MockSpinBox(str(board_size_upper_limit + 1))
+    expected_upper_row_value_message = (self.base_error_message
+                                        + "\n"
+                                        + Lg.lang["InvalidRowUpperLimit"]
+                                        + str(board_size_upper_limit))
+    
+    message = validate_inputs(invalid_upper_limit_value,
+                              numeric_value,
+                              numeric_value)
+    self.assertEqual(expected_upper_row_value_message,
+                     message,
+                     "Invalid upper row limit value error message")
+
+  def test_invalid_column_input(self):
+    board_size_lower_limit = BOARD_SIZE_VALUES["Small"][0]
+    board_size_upper_limit = BOARD_SIZE_VALUES["Big"][1]
+    numeric_value = self.MockSpinBox(str(board_size_lower_limit))
+    invalid_lower_limit_value = self.MockSpinBox(str(board_size_lower_limit - 1))
+
+    expected_lower_column_value_message = (self.base_error_message
+                                           + "\n"
+                                           + Lg.lang["InvalidColumnLowerLimit"]
+                                           + str(board_size_lower_limit))
+
+    message = validate_inputs(numeric_value,
+                              invalid_lower_limit_value,
+                              numeric_value)
+    self.assertEqual(expected_lower_column_value_message,
+                     message,
+                     "Invalid lower column limit value error message")
+
+    invalid_upper_limit_value = self.MockSpinBox(str(board_size_upper_limit + 1))
+    expected_upper_column_value_message = (self.base_error_message
+                                           + "\n"
+                                           + Lg.lang["InvalidColumnUpperLimit"]
+                                           + str(board_size_upper_limit))
+    
+    message = validate_inputs(numeric_value,
+                              invalid_upper_limit_value,
+                              numeric_value)
+    self.assertEqual(expected_upper_column_value_message,
+                     message,
+                     "Invalid upper column limit value error message")
