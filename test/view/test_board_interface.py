47a71cfc83d23ab6ff21be1d1f262f784b9c0ccb
Sebastián Londoño Escandón <sebastian.londono@farmu.com.co>
2026-09-07 18:06:31 -0500

    Added testing of function that reveals all broccolis

--- test/view/test_board_interface.py
+++ test/view/test_board_interface.py
@@ -3,7 +3,8 @@ import numpy as np
 import tkinter as tk
 from unittest.mock import patch
 
-from src.logic.board import Board
+from src.logic.constants.boardValues import GET_BOARD_VALUE
+from src.logic.board import Board, board_generator
 import src.view.boardInterface as boardInterface
 
 class TestChangeFlagStatus(unittest.TestCase):
@@ -90,6 +91,51 @@ class TestHandleFlagTile(unittest.TestCase):
       self.assertEqual(expected_counter,
                        boardInterface.broccoli_counter_value,
                        "Broccoli counter value should have been increase by 1")
+
+  def tearDown(self):
+    self.root.destroy()
+
+class TestHandleRevealBroccolis(unittest.TestCase):
+  def setUp(self):
+    self.root = tk.Tk()
+
+    size = 3
+    self.board_object = board_generator(size, size, 3)
+    self.buttons = np.empty(shape=[size, size], dtype="object")
+    self.button_text = "Broccoli"
+    for row in range(0, size):
+      for column in range(0, size):
+        button = tk.Button(self.root, text=self.button_text)
+        button.pack()
+        self.buttons[row, column] = button
+
+    tiles_board = self.board_object.tiles_board
+    rainbow_broccoli_pos = self.board_object.broccoli_positions[1]    
+    rainbow_broccoli_tile = tiles_board[rainbow_broccoli_pos[0],
+                                        rainbow_broccoli_pos[1]]
+    rainbow_broccoli_tile["tileValue"] = GET_BOARD_VALUE["rainbowBroccoli"]
+
+    flowering_broccoli_pos = self.board_object.broccoli_positions[2]
+    flowering_broccoli_tile = tiles_board[flowering_broccoli_pos[0],
+                                          flowering_broccoli_pos[1]]
+    flowering_broccoli_tile["tileValue"] = GET_BOARD_VALUE["floweringBroccoli"]
+
+  def test_handle_reveal_broccolis(self):
+    with (patch("src.view.boardInterface.open_pillow_image") as image,
+          ):
+      image.return_value = None
+
+      move_position = self.board_object.broccoli_positions[0]
+      boardInterface.handle_reveal_broccolis(self.board_object,
+                                             self.buttons,
+                                             move_position)
+
+      broccoli_positions = self.board_object.broccoli_positions
+      for pos in broccoli_positions:
+        new_text = self.buttons[pos[0], pos[1]].cget("text")
+        self.assertNotEqual(self.button_text,
+                            new_text,
+                            "Button text should have changed")
 
   def tearDown(self):
     self.root.destroy()
